1f6955e64acb76ed4d5a45fb3ff27ae565849d39
Marc Agas <marclambertagas@gmail.com>
2011-07-31 23:02:08 +0800

    apply design for right-col in pricing page

--- application/views/pricing/index.php
+++ application/views/pricing/index.php
@@ -1,4 +1,5 @@
 <?php
     $this->load->view('pricing/left_content');
     $this->load->view('pricing/main_content');
+    $this->load->view('pricing/right_content');
 ?>
--- application/views/pricing/right_content.php
+++ application/views/pricing/right_content.php
@@ -1,16 +1,24 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<div id="right-col">
+    <div id="save" class="box-cream">
+        <div class="bg-top-box-cream"></div>
+        <h3>SAVE $100</h3>
+        <p>Save $100 when you <a href="#">buy flyers</a> to go with your posters.</p>
+        <div class="bg-bottom-box-cream"></div>
+    </div>
+    <div id="which-size" class="box-cream">
+        <div class="bg-top-box-cream"></div>
+        <h3>Which Size To Buy</h3>
+        <p>If you don't know which size to buy, here are <a href="#">some plain English hints!</a></p>
+        <div class="bg-bottom-box-cream"></div>
+    </div>
+    <div id="testimonials">
+        <div class="bg-top-box"></div>
+        <h3 class="bg-color">Testimonials</h3>
+        <p class="description">Top Job! Thanks very much &amp; we'll be contacting you the second we have another job &amp; also telling everyone we know about you.</p>
+        <p class="author">John Zak</p>
+        <p class="description">Top Job! Thanks very much &amp; we'll be contacting you the second we have another job &amp; also telling everyone we know about you.</p>
+        <p class="author">John Zak</p>
+        <p class="description">Top Job! Thanks very much &amp; we'll be contacting you the second we have another job &amp; also telling everyone we know about you.</p>
+        <p class="author">John Zak</p>
+    </div>
+</div>
